1bb96badab2b4041bd095b7cb4f7923ee5fb2e27
博麗靈夢 <1725338233@qq.com>
2022-05-08 19:56:22 +0800

    增加 BeanUtils, 设置 Gradle 插件镜像
    Signed-off-by: 博麗靈夢 <1725338233@qq.com>

--- build.gradle.kts
+++ build.gradle.kts
@@ -23,6 +23,7 @@ dependencies {
     implementation("org.apache.logging.log4j:log4j-api:2.17.0")
     implementation("org.apache.logging.log4j:log4j-core:2.17.2")
     implementation("org.apache.logging.log4j:log4j-slf4j-impl:2.17.2")
+    implementation("commons-beanutils:commons-beanutils:1.9.4")
 
     testImplementation(kotlin("test"))
     implementation(kotlin("reflect"))
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,2 +1,11 @@
+pluginManagement {
+    repositories {
+        maven {
+            name = "阿里云 gradle 插件镜像"
+            setUrl("https://maven.aliyun.com/repository/gradle-plugin")
+        }
+    }
+}
+
 rootProject.name = "驾考宝典"
 
