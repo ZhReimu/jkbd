6cf1f34d810a7bef88a84afd8ca3242c49a90d61
博麗靈夢 <1725338233@qq.com>
2022-05-09 13:35:20 +0800

    优化题目显示
    Signed-off-by: 博麗靈夢 <1725338233@qq.com>

--- src/main/kotlin/com/mrx/jkbd/entity/DTOQuestion.kt
+++ src/main/kotlin/com/mrx/jkbd/entity/DTOQuestion.kt
@@ -5,4 +5,22 @@ package com.mrx.jkbd.entity
  * @since 2022-05-08-0008
  **/
 class DTOQuestion : BaseQuestion() {
+
+    fun getStringQuestion(id: Int): String {
+        val decodedQuestion = getDecodedField(this::question)
+        val sb = StringBuilder("第 $id 题: $decodedQuestion\n")
+        sb.append("A: $optionA\n").append("B: $optionB\n")
+        // optionType 0 为 判断
+        return if (optionType == 0) {
+            sb.toString()
+        } else {
+            optionC?.let { sb.append("C: $it\n") }
+            optionD?.let { sb.append("D: $it\n") }
+            optionE?.let { sb.append("E: $it\n") }
+            optionF?.let { sb.append("F: $it\n") }
+            optionG?.let { sb.append("G: $it\n") }
+            sb.toString()
+        }
+    }
+
 }
